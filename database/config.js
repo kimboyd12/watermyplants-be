390b92d639a193bafc5d727c9b5895675819e041
kim-boyd <kimberlyboyd2@gmail.com>
2021-04-12 18:55:46 -0400

    having issue with node_env

--- database/config.js
+++ database/config.js
@@ -1,3 +1,5 @@
+
+
 const knex = require("knex");
 const knexConfig = require("../knexfile.js");
 
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
+// require("dotenv").config();
 
 const express = require("express")
 const cors = require("cors")
 const helmet = require("helmet")
 const session = require("express-session")
 
-// const usersRouter = require("./users/users-router")
-// const plantsRouter = require("./plants/plants-router")
+const usersRouter = require("./users/users-router")
+const plantsRouter = require("./plants/plants-router")
 const welcomeRouter = require("./welcome-router")
 
 const server = express()
@@ -15,14 +15,14 @@ server.use(helmet())
 server.use(cors())
 server.use(express.json())
 server.use(session({
-    resave:false,
+    resave: false,
     saveUninitialized: false,
     secret: process.env.JWT_SECRET,
 }))
 
 server.use("/", welcomeRouter)
-// server.use("/users", usersRouter)
-// server.use("/plants", plantsRouter)
+server.use("/users", usersRouter)
+server.use("/plants", plantsRouter)
 
 server.use((err, req, res, next) => {
     console.log(err)
